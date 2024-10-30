2486ed662ac728efdc69ae7a77f1992d5a5a8b4d
Aditya Sharma <114848909+Aditya1or0@users.noreply.github.com>
2024-10-30 18:52:17 +0530

    Update Product.jsx

--- Frontend/src/pages/Product.jsx
+++ Frontend/src/pages/Product.jsx
@@ -1,10 +1,11 @@
 import React, { useContext, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
 import { assets } from "../assets/assets";
 import RelatedProducts from "../components/RelatedProducts";
 
 const Product = () => {
+  const navigate = useNavigate();
   const { productId } = useParams();
   const { products, currency, addToCart } = useContext(ShopContext);
   const [productData, setProductData] = useState(false);

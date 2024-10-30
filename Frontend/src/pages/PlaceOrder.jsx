09e19d84654c19473c3117db13f09a5a7f5f348f
Aditya Sharma <114848909+Aditya1or0@users.noreply.github.com>
2024-10-30 18:48:18 +0530

    Update PlaceOrder.jsx

--- Frontend/src/pages/PlaceOrder.jsx
+++ Frontend/src/pages/PlaceOrder.jsx
@@ -183,7 +183,7 @@ const PlaceOrder = () => {
       className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t"
     >
       {/* Left Side */}
-      <div className="flex flex-col gap-4 w-full sm:max-w-[480px]">
+      <div className="flex flex-col gap-4 w-full sm:max-w-[480px] text-gray-800">
         <div className="text-xl sm:text-2xl my-3">
           <Title text1={"DELIVERY"} text2={" INFORMATION"} />
         </div>

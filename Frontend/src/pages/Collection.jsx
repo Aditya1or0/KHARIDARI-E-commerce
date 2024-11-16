e58a2af03ed9fe221775462d70834274c5b4de76
Aditya Sharma <adityapandit264@gmail.com>
2024-11-16 21:07:58 +0530

    Update Collection.jsx

--- Frontend/src/pages/Collection.jsx
+++ Frontend/src/pages/Collection.jsx
@@ -19,6 +19,7 @@ const Collection = () => {
       setCategory((prev) => [...prev, e.target.value]);
     }
   };
+
   const toggleSubCategory = (e) => {
     if (subCategory.includes(e.target.value)) {
       setSubCategory((prev) => prev.filter((item) => item !== e.target.value));
@@ -61,6 +62,7 @@ const Collection = () => {
         break;
     }
   };
+
   useEffect(() => {
     sortProduct();
   }, [sortType]);
@@ -69,127 +71,133 @@ const Collection = () => {
     applyFilter();
   }, [category, subCategory, search, showSearch, products]);
 
-  return ( 
-    {/*Left Side*/}
-    <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
-      {/*Filter Options*/}
-      <div className="min-w-60">
-        <p
-          onClick={() => setShowFilter(!showFilter)}
-          className="my-2 text-xl flex items-center cursor-pointer gap-2 dark:text-gray-200"
-        >
-          FILTERS
-          <img
-            src={assets.dropdown_icon}
-            className={`h-3 sm:hidden ${showFilter ? "rotate-90" : ""}`}
-            alt=""
-          />
-        </p>
-        {/* Category Filter*/}
-        <div
-          className={`border border-gray-300 pl-5 py-3 mt-6 ${
-            showFilter ? "" : "hidden"
-          } sm:block`}
-        >
-          <p className="mb-3 text-sm font-medium dark:text-gray-300">
-            CATEGORIES
-          </p>
-          <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
-            <p className="flex gap-2 dark:text-gray-400">
-              <input
-                className="w-3"
-                type="checkbox"
-                value={"Men"}
-                onChange={toggleCategory}
-              />
-              Men
-            </p>
-            <p className="flex gap-2 dark:text-gray-400">
-              <input
-                className="w-3"
-                type="checkbox"
-                value={"Women"}
-                onChange={toggleCategory}
-              />
-              Women
-            </p>
-            <p className="flex gap-2 dark:text-gray-400">
-              <input
-                className="w-3"
-                type="checkbox"
-                value={"Kids"}
-                onChange={toggleCategory}
-              />
-              Kids
-            </p>
-          </div>
-        </div>
-        {/*Subcategory Filter*/}
-        <div
-          className={`border border-gray-300 pl-5 py-3  my-5 ${
-            showFilter ? "" : "hidden"
-          } sm:block`}
-        >
-          <p className="mb-3 text-sm font-medium dark:text-gray-300">TYPE</p>
-          <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
-            <p className="flex gap-2 dark:text-gray-400">
-              <input
-                className="w-3"
-                type="checkbox"
-                value={"Topwear"}
-                onChange={toggleSubCategory}
-              />
-              Topwear
-            </p>
-            <p className="flex gap-2 dark:text-gray-400">
-              <input
-                className="w-3"
-                type="checkbox"
-                value={"Bottomwear"}
-                onChange={toggleSubCategory}
-              />
-              Bottomwear
-            </p>
-            <p className="flex gap-2 dark:text-gray-400">
-              <input
-                className="w-3"
-                type="checkbox"
-                value={"Winterwear"}
-                onChange={toggleSubCategory}
-              />
-              Winterwear
-            </p>
-          </div>
-        </div>
-      </div>
-      {/*Right Side*/}
-      <div className="flex-1">
-        <div className="flex justify-between text-base sm:text-2xl mb-4">
-          <Title text1={"ALL"} text2={" COLLECTIONS"} />
-          {/*Sort Options*/}
-          <select
-            className="border-2 border-gray-300 text-sm px-2 py-1 w-full max-w-xs dark:text-gray-300 dark:bg-[#111827] dark:border-gray-400"
-            onChange={(e) => setSortType(e.target.value)}
+  return (
+    <>
+      {/* Left Side */}
+      <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
+        {/* Filter Options */}
+        <div className="min-w-60">
+          <p
+            onClick={() => setShowFilter(!showFilter)}
+            className="my-2 text-xl flex items-center cursor-pointer gap-2 dark:text-gray-200"
           >
-            <option value="relevent ">Sort by: Relevant</option>
-            <option value="low-high">Sort by: Low to High</option>
-            <option value="high-low">Sort by:High to Low</option>
-          </select>
-        </div>
-        {/*Products*/}
-        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6">
-          {filterProducts.map((item, index) => (
-            <ProductItem
-              key={index}
-              id={item._id}
-              image={item.image}
-              name={item.name}
-              price={item.price}
+            FILTERS
+            <img
+              src={assets.dropdown_icon}
+              className={`h-3 sm:hidden ${showFilter ? "rotate-90" : ""}`}
+              alt=""
             />
-          ))}
+          </p>
+
+          {/* Category Filter */}
+          <div
+            className={`border border-gray-300 pl-5 py-3 mt-6 ${
+              showFilter ? "" : "hidden"
+            } sm:block`}
+          >
+            <p className="mb-3 text-sm font-medium dark:text-gray-300">
+              CATEGORIES
+            </p>
+            <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
+              <p className="flex gap-2 dark:text-gray-400">
+                <input
+                  className="w-3"
+                  type="checkbox"
+                  value={"Men"}
+                  onChange={toggleCategory}
+                />
+                Men
+              </p>
+              <p className="flex gap-2 dark:text-gray-400">
+                <input
+                  className="w-3"
+                  type="checkbox"
+                  value={"Women"}
+                  onChange={toggleCategory}
+                />
+                Women
+              </p>
+              <p className="flex gap-2 dark:text-gray-400">
+                <input
+                  className="w-3"
+                  type="checkbox"
+                  value={"Kids"}
+                  onChange={toggleCategory}
+                />
+                Kids
+              </p>
+            </div>
+          </div>
+
+          {/* Subcategory Filter */}
+          <div
+            className={`border border-gray-300 pl-5 py-3 my-5 ${
+              showFilter ? "" : "hidden"
+            } sm:block`}
+          >
+            <p className="mb-3 text-sm font-medium dark:text-gray-300">TYPE</p>
+            <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
+              <p className="flex gap-2 dark:text-gray-400">
+                <input
+                  className="w-3"
+                  type="checkbox"
+                  value={"Topwear"}
+                  onChange={toggleSubCategory}
+                />
+                Topwear
+              </p>
+              <p className="flex gap-2 dark:text-gray-400">
+                <input
+                  className="w-3"
+                  type="checkbox"
+                  value={"Bottomwear"}
+                  onChange={toggleSubCategory}
+                />
+                Bottomwear
+              </p>
+              <p className="flex gap-2 dark:text-gray-400">
+                <input
+                  className="w-3"
+                  type="checkbox"
+                  value={"Winterwear"}
+                  onChange={toggleSubCategory}
+                />
+                Winterwear
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* Right Side */}
+        <div className="flex-1">
+          <div className="flex justify-between text-base sm:text-2xl mb-4">
+            <Title text1={"ALL"} text2={" COLLECTIONS"} />
+            {/* Sort Options */}
+            <select
+              className="border-2 border-gray-300 text-sm px-2 py-1 w-full max-w-xs dark:text-gray-300 dark:bg-[#111827] dark:border-gray-400"
+              onChange={(e) => setSortType(e.target.value)}
+            >
+              <option value="relevent">Sort by: Relevant</option>
+              <option value="low-high">Sort by: Low to High</option>
+              <option value="high-low">Sort by: High to Low</option>
+            </select>
+          </div>
+
+          {/* Products */}
+          <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6">
+            {filterProducts.map((item, index) => (
+              <ProductItem
+                key={index}
+                id={item._id}
+                image={item.image}
+                name={item.name}
+                price={item.price}
+              />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
